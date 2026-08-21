e0f55cb6b769d49a6ad6bb1417737a7e5bbaec1b
Srinath <srinath9787672574@gmail.com>
2026-08-21 19:22:15 +0530

    Add files via upload

---
+++ start.java
@@ -0,0 +1,9 @@
+public class start{
+public static void main(String[]args) {
+    int n =5;
+   for(int row=1;row<=n;row++){
+   for(int col=1;col<=row;col++){
+   System.out.print("*"+ " ");
+   }
+   System.out.println();
+}}}
